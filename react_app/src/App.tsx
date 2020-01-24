926168da312a735c48ed96e912a1691d0dbfe102
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-01-24 19:12:49 +0100

    feat(app): create input and button

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,13 +1,30 @@
-import React, { FunctionComponent } from "react";
+import React, { useState, FunctionComponent } from "react";
+import {connect, DispatchProp} from 'react-redux';
 import "./style.scss";
 // actions
+import { valuesActions } from './@features/store/values';
 
-const App: FunctionComponent<any> = () => {
+const App: FunctionComponent<any> = (props) => {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const onSubmit = () => {
+      props.dispatch(valuesActions.getPersonValues(inputValue));
+    };
+
   return (
     <>
-      Hello from app
+      <h1>Alpha number calc!</h1>
+      <p>Get your alpha character calculation</p>
+      <input type="text" value={inputValue} onChange={(ev) => setInputValue(ev.target.value)}/>
+      <button onClick={onSubmit}>Proceed</button>
     </>
   );
 }
 
-export default App;
+const mapStateToProps = (state: any) => ({
+  isLoading: state.values.isLoading,
+})
+
+const AppContainer = connect(mapStateToProps)(App);
+
+export default AppContainer;
