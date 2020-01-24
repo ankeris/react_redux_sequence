8fb09a26f999b6a82a927aee8a7dcc9e7a602597
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-01-24 20:00:28 +0100

    feat(global): add shared types for API+APP

---
+++ shared_types/types.ts
@@ -0,0 +1,13 @@
+export class Person {
+    val1: number;
+    val2: number; 
+}
+
+export class Facility {
+    val3: number;
+    val4: number; 
+}
+
+export class Exposure {
+    val5: number;
+}
